de7bbb1bcdcd771af96e00b0aab865b4b3028d09
Pancakes13 <neil.llenes@gmail.com>
2018-09-16 21:16:59 +0800

    Added SweetAlert2 assets
    Added css and js files for sweetalert2

--- assets/stylesheets.php
+++ assets/stylesheets.php
@@ -16,12 +16,13 @@
     <link rel="stylesheet" href="../assets/css/cs-skin-elastic.css">
     <link rel="stylesheet" href="../assets/css/lib/datatable/dataTables.bootstrap.min.css">
     <!-- <link rel="stylesheet" href="assets/css/bootstrap-select.less"> -->
+    <link rel="stylesheet" href="../assets/css/sweetalert2.min.css">
     <link rel="stylesheet" href="../assets/scss/style.css">
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
     <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
     <script src="../assets/js/vendor/jquery-3.3.1.min.js"></script>
-
+    <script src="../assets/js/sweetalert2.min.js"></script>
 
 </head>
