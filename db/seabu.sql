adf4fad99190aabf926a0e4343ede230bd7af08c
Pancakes13 <neil.llenes@gmail.com>
2018-09-17 22:56:03 +0800

    Made changes to DB
    Added description to log tables. Added type to item_line

--- db/seabu.sql
+++ db/seabu.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 14, 2018 at 03:21 PM
+-- Generation Time: Sep 17, 2018 at 04:53 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.5.38
 
@@ -40,6 +40,13 @@ CREATE TABLE `employee` (
   `isDeleted` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`employee_id`, `username`, `first_name`, `last_name`, `middle_name`, `pass`, `email`, `contact_no`, `birthdate`, `picture`, `isDeleted`) VALUES
+(1, 'neily', 'neil', 'llenes', 'diaz', '123', 'nllenes@gmail.com', NULL, '1997-11-13 00:00:00', NULL, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -48,7 +55,9 @@ CREATE TABLE `employee` (
 
 CREATE TABLE `employee_log` (
   `employee_log_id` int(11) NOT NULL,
-  `log_timestamp` datetime NOT NULL,
+  `action` enum('"create"','"update"','"delete"','') NOT NULL,
+  `log_description` varchar(254) NOT NULL,
+  `log_timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `employee_id` int(11) DEFAULT NULL,
   `performed_by` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -81,6 +90,43 @@ CREATE TABLE `item` (
   `isDeleted` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`item_id`, `name`, `price`, `qty`, `isDeleted`) VALUES
+(1, 'Cheezy Scallop', 175, 0, 0),
+(2, 'Crab', 120, 0, 0),
+(3, 'Lobster', 175, 0, 0),
+(4, 'Shrimp', 150, 0, 0),
+(5, 'Calamares', 230, 0, 0),
+(6, 'Foods', 112, 0, 0),
+(7, 'fsdjfsldkf', 111, 0, 0),
+(8, 'sadfsad', 111, 0, 0),
+(9, 'sadfds', 111, 0, 0),
+(10, 'faa', 12, 0, 0),
+(11, 'bn', 222, 0, 1),
+(12, 'lumpia', 300, 0, 0),
+(13, 'cho', 109, 0, 0),
+(14, 'aaa', 111, 0, 1),
+(15, 'bb', 111, 0, 0),
+(16, 'ccc', 222, 0, 1),
+(17, 'abc', 123, 0, 1),
+(18, 'aaaa', 123, 0, 1),
+(19, 'aa', 22, 0, 0),
+(20, 'a', 1, 0, 1),
+(21, 'a', 1, 0, 1),
+(22, 'aaaa', 11111, 0, 1),
+(23, '11111', 55555, 0, 0),
+(24, 'a', 123, 0, 1),
+(25, 'a', 123, 0, 1),
+(26, 'a', 54321, 0, 0),
+(27, 'zoo', 200, 0, 0),
+(28, 'zoo', 200, 0, 0),
+(29, 'zoo', 199, 0, 1),
+(30, 'zoo', 199, 0, 0),
+(31, '123', 123, 0, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -91,6 +137,7 @@ CREATE TABLE `item_line` (
   `item_line_id` int(11) NOT NULL,
   `price` float NOT NULL,
   `qty` int(11) NOT NULL,
+  `item_line_type` enum('"local"','"honestbee"','','') NOT NULL,
   `stock_transaction_id` int(11) DEFAULT NULL,
   `item_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -103,11 +150,21 @@ CREATE TABLE `item_line` (
 
 CREATE TABLE `item_log` (
   `item_log_id` int(11) NOT NULL,
-  `log_timestamp` datetime NOT NULL,
+  `log_timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `log_action` enum('Create','Update','Delete') NOT NULL,
+  `log_description` varchar(254) DEFAULT NULL,
   `employee_id` int(11) DEFAULT NULL,
   `item_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `item_log`
+--
+
+INSERT INTO `item_log` (`item_log_id`, `log_timestamp`, `log_action`, `log_description`, `employee_id`, `item_id`) VALUES
+(1, '2018-09-16 13:10:47', 'Create', '', 1, 1),
+(3, '2018-09-16 21:22:13', 'Delete', '', 1, 17),
+(5, '2018-09-16 21:27:17', 'Delete', '', 1, 31);
 
 -- --------------------------------------------------------
 
@@ -185,7 +242,7 @@ ALTER TABLE `stock_transaction`
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `employee_log`
 --
@@ -200,7 +257,7 @@ ALTER TABLE `expense`
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 --
 -- AUTO_INCREMENT for table `item_line`
 --
@@ -210,7 +267,7 @@ ALTER TABLE `item_line`
 -- AUTO_INCREMENT for table `item_log`
 --
 ALTER TABLE `item_log`
-  MODIFY `item_log_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `item_log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `stock_transaction`
 --
