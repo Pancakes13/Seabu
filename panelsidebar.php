183c259d5d2cc0e0d100f26807bf79dc75e5a1a7
Fratz Antigua <fratzantigua@gmail.com>
2018-09-23 23:49:20 +0800

    color changed sidebar

--- panelsidebar.php
+++ panelsidebar.php
@@ -6,14 +6,14 @@
 <body>
         <!-- Left Panel -->
 
-    <aside id="left-panel" class="left-panel">
-        <nav class="navbar navbar-expand-sm navbar-default">
+    <aside id="left-panel" class="left-panel" style="background-color: #076369;">
+        <nav class="navbar navbar-expand-sm navbar-default"style="background-color: #076369;">
 
-            <div class="navbar-header">
+            <div class="navbar-header" style="background-color: white;">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fa fa-bars"></i>
                 </button>
-                <a class="navbar-brand" href="./"><img src="../images/logo.png" alt="Logo"></a>
+                <a class="navbar-brand" href="./"><img src="../images/seabu-logo.png" alt="Logo"></a>
                 <a class="navbar-brand hidden" href="./"><img src="../images/logo2.png" alt="Logo"></a>
             </div>
 
@@ -22,7 +22,7 @@
                     <li class="active">
                         <a href="home.php"> <i class="menu-icon fa fa-home"></i>Home </a>
                     </li>
-                    <h3 class="menu-title">Menu</h3><!-- /.menu-title -->
+                    <h3 class="menu-title" style="color: white;">Menu</h3><!-- /.menu-title -->
                     <!-- Tree Menu sample
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Tally</a>
