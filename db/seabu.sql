a5cecd04210776bd3833a025f1ea9022ae96a0fd
Pancakes13 <neil.llenes@gmail.com>
2018-10-18 23:10:43 +0800

    Added latest database. Performed seeding

--- db/seabu.sql
+++ db/seabu.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2018 at 11:40 PM
+-- Generation Time: Oct 18, 2018 at 05:10 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.5.38
 
@@ -44,9 +44,10 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`employee_id`, `username`, `first_name`, `last_name`, `middle_name`, `pass`, `email`, `contact_no`, `birthdate`, `isDeleted`) VALUES
-(1, 'neily', 'neil', 'llenes', 'diaz', '123', 'nllenes@gmail.com', '09111111111', '1997-11-13', 0),
+(1, 'neily', 'Neil', 'Llenes', 'Diaz', '123', 'neilllenes@gmail.com', '09111111111', '1997-11-13', 0),
 (2, 'sammyj', 'Samuel', 'Jones', 'James', '123', 'samjones@gmail.com', '09991234567', '1990-09-13', 0),
-(3, 'sadfsad', 'sadfsadf', 'sdafsad', 'sadfasdf', '123', 'sdafsf@gmail.com', 'kejfwlf', '1111-11-11', 1);
+(3, 'sadfsad', 'sadfsadf', 'sdafsad', 'sadfasdf', '123', 'sdafsf@gmail.com', 'kejfwlf', '1111-11-11', 1),
+(4, 'Jdoe', 'John', 'Doe', 'James', '123', 'jdoe@gmail.com', '09999876543', '1987-12-18', 0);
 
 -- --------------------------------------------------------
 
@@ -72,7 +73,10 @@ INSERT INTO `employee_log` (`employee_log_id`, `action`, `log_description`, `log
 (2, 'Create', 'Employee was Created', '2018-09-24 22:37:53', 1, 1),
 (3, 'Delete', 'Employee was deleted', '2018-09-24 22:40:12', 3, 1),
 (4, 'Update', 'Employee was manually updated', '2018-09-25 22:59:29', 2, 1),
-(5, 'Update', 'Employee was manually updated', '2018-09-25 23:01:43', 2, 1);
+(5, 'Update', 'Employee was manually updated', '2018-09-25 23:01:43', 2, 1),
+(6, 'Update', 'Employee was manually updated', '2018-10-10 21:59:58', 1, 1),
+(7, 'Create', 'Employee was Created', '2018-10-15 21:38:27', 4, 1),
+(8, 'Update', 'Employee was manually updated', '2018-10-15 21:38:46', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -95,7 +99,12 @@ CREATE TABLE `expense` (
 
 INSERT INTO `expense` (`expense_id`, `name`, `description`, `price`, `expense_timestamp`, `employee_id`) VALUES
 (3, 'Electricity Bill', 'Paid Electricity Bill for September 2018', 8750, '2018-09-23 18:46:21', 1),
-(4, 'Water Bill', 'Paid water bill for the month', 3420, '2018-09-23 19:04:20', 1);
+(4, 'Water Bill', 'Paid water bill for the month', 3420, '2018-09-23 19:04:20', 1),
+(5, 'Expense', 'A test expense', 10100, '2018-10-07 20:23:16', 1),
+(6, 'Expense2', 'A test expense', 1500, '2018-10-07 20:23:33', 1),
+(7, 'Expense3', 'A test expense', 37500, '2018-10-07 20:23:44', 1),
+(8, 'Stove repair', 'Stove required new parts. Plus labor fee', 3500, '2018-10-15 21:39:33', 1),
+(10, 'aaa', 'bbb', 222, '2018-10-15 21:40:09', 1);
 
 -- --------------------------------------------------------
 
@@ -116,19 +125,19 @@ CREATE TABLE `item` (
 --
 
 INSERT INTO `item` (`item_id`, `name`, `price`, `qty`, `isDeleted`) VALUES
-(1, 'Cheezy Scallop', 175, 0, 0),
+(1, 'Cheezy Scallop', 179, 11, 0),
 (2, '', 0, 0, 1),
-(3, 'Lobster', 175, 0, 0),
-(4, 'Shrimp', 150, 0, 0),
-(5, 'Calamares', 230, 0, 0),
-(6, 'Foods', 112, 0, 0),
+(3, 'Lobster', 175, 7, 0),
+(4, 'Shrimp', 150, 17, 0),
+(5, 'Calamares', 250, 16, 0),
+(6, 'Foods', 112, 5, 0),
 (7, 'fsdjfsldkf', 111, 0, 1),
 (8, 'sadfsad', 111, 0, 1),
 (9, 'sadfds', 111, 0, 1),
 (10, 'faa', 12, 0, 1),
 (11, 'bn', 222, 0, 1),
 (12, 'lumpia', 300, 0, 0),
-(13, 'Lechon', 250, 0, 0),
+(13, 'Lechon', 250, 4, 0),
 (14, 'aaa', 111, 0, 1),
 (15, 'bb', 111, 0, 1),
 (16, 'ccc', 222, 0, 1),
@@ -148,8 +157,8 @@ INSERT INTO `item` (`item_id`, `name`, `price`, `qty`, `isDeleted`) VALUES
 (30, 'zoo', 199, 0, 1),
 (31, '123', 123, 0, 1),
 (32, 'Yummy food', 120, 0, 1),
-(33, 'Fried Chicken (4 pc)', 280, 5, 0),
-(34, 'Nachos', 234, 0, 0);
+(33, 'Fried Chicken (4 pc)', 280, 1, 0),
+(34, 'Nachos', 234, 7, 0);
 
 -- --------------------------------------------------------
 
@@ -161,7 +170,7 @@ CREATE TABLE `item_line` (
   `item_line_id` int(11) NOT NULL,
   `price` float NOT NULL,
   `qty` int(11) NOT NULL,
-  `item_line_type` enum('local','honestbee') NOT NULL,
+  `item_line_type` enum('local','honestbee') DEFAULT NULL,
   `stock_transaction_id` int(11) DEFAULT NULL,
   `item_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -188,7 +197,34 @@ INSERT INTO `item_line` (`item_line_id`, `price`, `qty`, `item_line_type`, `stoc
 (36, 150, 1, 'local', 22, 4),
 (37, 300, 1, 'honestbee', 22, 12),
 (38, 280, 1, 'honestbee', 22, 33),
-(39, 234, 1, 'local', 22, 34);
+(39, 234, 1, 'local', 22, 34),
+(40, 175, 2, 'local', 23, 1),
+(41, 150, 2, 'local', 23, 4),
+(42, 112, 1, 'local', 23, 6),
+(43, 250, 3, 'local', 23, 13),
+(44, 280, 1, 'local', 23, 33),
+(51, 150, 2, 'local', 27, 4),
+(52, 300, 2, 'local', 27, 12),
+(54, 280, 1, 'local', 27, 33),
+(55, 175, 3, 'local', 28, 1),
+(56, 175, 9, 'local', 29, 1),
+(59, 0, 4, NULL, 35, 3),
+(60, 0, -2, NULL, 36, 5),
+(61, 179, 2, 'local', 37, 1),
+(62, 250, 2, 'local', 37, 5),
+(63, 280, 1, 'local', 37, 33),
+(64, 250, 2, 'local', 38, 5),
+(65, 175, 2, 'honestbee', 39, 3),
+(66, 179, 1, 'local', 39, 1),
+(67, 250, 2, 'local', 39, 5),
+(68, 250, 1, 'local', 39, 13),
+(69, 280, 2, 'local', 39, 33),
+(70, 0, 16, NULL, 40, 5),
+(71, 0, 5, NULL, 41, 6),
+(72, 0, 8, NULL, 42, 1),
+(73, 0, 7, NULL, 43, 3),
+(74, 0, 7, NULL, 44, 34),
+(75, 0, 17, NULL, 45, 4);
 
 -- --------------------------------------------------------
 
@@ -231,7 +267,11 @@ INSERT INTO `item_log` (`item_log_id`, `log_timestamp`, `log_action`, `log_descr
 (24, '2018-09-23 15:13:53', 'Delete', 'Item was deleted', 1, 30),
 (25, '2018-09-24 22:35:40', 'Delete', 'Item was deleted', 1, 2),
 (26, '2018-09-25 22:57:05', 'Update', 'Item was manually updated', 1, 2),
-(27, '2018-09-25 23:05:17', 'Create', 'Item was Created', 1, 34);
+(27, '2018-09-25 23:05:17', 'Create', 'Item was Created', 1, 34),
+(28, '2018-10-10 21:55:01', 'Update', 'Item was manually updated', 1, 1),
+(29, '2018-10-10 21:55:25', 'Update', 'Item was manually updated', 1, 1),
+(30, '2018-10-10 21:55:40', 'Update', 'Item was manually updated', 1, 1),
+(31, '2018-10-14 20:32:36', 'Update', 'Item was manually updated', 1, 5);
 
 -- --------------------------------------------------------
 
@@ -256,7 +296,22 @@ INSERT INTO `stock_transaction` (`stock_transaction_id`, `transaction_timestamp`
 (15, '2018-09-30 21:12:59', 'Sold', 1),
 (16, '2018-10-02 22:28:02', 'Sold', 1),
 (18, '2018-10-03 21:58:55', 'Sold', 1),
-(22, '2018-10-04 21:49:46', 'Sold', 1);
+(22, '2018-10-04 21:49:46', 'Sold', 1),
+(23, '2018-10-06 18:43:33', 'Sold', 1),
+(27, '2018-10-07 20:40:13', 'Sold', 1),
+(28, '2018-10-09 21:18:38', 'Sold', 1),
+(29, '2018-10-10 21:41:37', 'Sold', 1),
+(35, '2018-10-12 23:50:09', 'Restock', 1),
+(36, '2018-10-14 19:40:42', 'Damaged', 1),
+(37, '2018-10-15 21:36:45', 'Sold', 1),
+(38, '2018-10-17 21:34:40', 'Sold', 1),
+(39, '2018-10-18 22:19:45', 'Sold', 1),
+(40, '2018-10-18 23:00:04', 'Restock', 1),
+(41, '2018-10-18 23:01:16', 'Restock', 1),
+(42, '2018-10-18 23:01:22', 'Restock', 1),
+(43, '2018-10-18 23:01:27', 'Restock', 1),
+(44, '2018-10-18 23:01:32', 'Restock', 1),
+(45, '2018-10-18 23:01:38', 'Restock', 1);
 
 --
 -- Indexes for dumped tables
@@ -321,17 +376,17 @@ ALTER TABLE `stock_transaction`
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `employee_log`
 --
 ALTER TABLE `employee_log`
-  MODIFY `employee_log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `employee_log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `expense`
 --
 ALTER TABLE `expense`
-  MODIFY `expense_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `expense_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `item`
 --
@@ -341,17 +396,17 @@ ALTER TABLE `item`
 -- AUTO_INCREMENT for table `item_line`
 --
 ALTER TABLE `item_line`
-  MODIFY `item_line_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `item_line_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 --
 -- AUTO_INCREMENT for table `item_log`
 --
 ALTER TABLE `item_log`
-  MODIFY `item_log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `item_log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 --
 -- AUTO_INCREMENT for table `stock_transaction`
 --
 ALTER TABLE `stock_transaction`
-  MODIFY `stock_transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `stock_transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 --
 -- Constraints for dumped tables
 --
