41d56455fd61b73f8121a2d5530550a5486ed28d
Pancakes13 <neil.llenes@gmail.com>
2019-04-08 22:25:08 +0800

    Fixed main search stock usage method

--- queries/get/searchStockUsageLog.php
+++ queries/get/searchStockUsageLog.php
@@ -5,24 +5,26 @@ $date2 = $_POST['date2'];
 $id = $_POST['branch_id'];
 
 $result = $conn->query("SELECT `il`.`item_line_id`, `i`.`item_id`, 
-                `i`.`name`, `il`.`old_stock`, `i`.`qty` AS 'item_qty', 
-                `il`.`price`, `il`.`qty`, `il`.`item_line_type`,
-                `s`.`transaction_timestamp`, `s`.`type`, `s`.`transaction_timestamp`, 
+                `i`.`name`, `il`.`old_stock`, `bi`.`qty` AS 'item_qty', 
+                `il`.`price`, `il`.`qty`, `s`.`transaction_timestamp`,
+                `s`.`type`, `s`.`transaction_timestamp`, 
                 `e`.`first_name`, `e`.`last_name`, DATE(`s`.`transaction_timestamp`) AS `dateToday`,
                 `b`.`branch_id`, `b`.`name` AS `branch_name`
                 FROM `stock_transaction` `s` 
                 INNER JOIN `item_line` `il`
                 ON  `s`.`stock_transaction_id` = `il`.`stock_transaction_id`
+                INNER JOIN `branch_item` `bi`
+                ON `il`.`branch_item_id` = `bi`.`branch_item_id`
                 INNER JOIN `item` `i`
-                ON `il`.`item_id` = `i`.`item_id`
+                ON `bi`.`item_id` = `i`.`item_id`
                 INNER JOIN `employee` `e`
                 ON `e`.`employee_id` = `s`.`employee_id`
                 INNER JOIN `branch` `b`
                 ON `e`.`branch_id` = `b`.`branch_id`
                 AND DATE(`s`.`transaction_timestamp`) >= '$date1' AND DATE(`s`.`transaction_timestamp`) <= '$date2'
                 AND `s`.`isVoid` = 0
-                AND `i`.`branch_id` = $id
-                AND `i`.`branch_id` != 10
+                AND `bi`.`branch_id` = $id
+                AND `bi`.`branch_id` != 1
                 ORDER BY `s`.`transaction_timestamp` DESC");
 
 $result_array = array();
